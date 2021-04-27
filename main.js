ec52f0936b289e1c6f5f2bbf90ba4d5691660fcd
Pranav Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-04-27 13:32:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,51 @@ Webcam.set({
 Webcam.attach("#camera");
 
 function takeSnapshot() {
-    Webcam.snap(function(data_uri) {
-        document.getElementById("snapshot").innerHTML = '<img src="'+data_uri+'"/>';
-    })
+  Webcam.snap(function (data_uri) {
+    document.getElementById("snapshot").innerHTML =
+      '<img id="image" src="' + data_uri + '"/>';
+  });
 }
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/FZt2qDgPC/model.json", modelLoaded);
+classifier = ml5.imageClassifier(
+  "https://teachablemachine.withgoogle.com/models/FZt2qDgPC/model.json",
+  modelLoaded
+);
 
 function modelLoaded() {
   console.log(ml5.version);
+}
+
+function speak(pred) {
+  var synth = window.speechSynthesis;
+  text1 = "My prediction is " + pred;
+  var utterThis = new SpeechSynthesisUtterance(text1);
+  synth.speak(utterThis);
+}
+
+function check() {
+  var myImg = document.getElementById("image");
+  classifier.classify(myImg, getData);
+}
+
+function getData(error, result) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(result);
+    var label = result[0].label;
+    document.getElementById("pred").innerHTML = showGesture(label);
+
+    speak(label);
+  }
+}
+
+function showGesture(label) {
+  if (label == "Amazing") {
+    return "&#128076;";
+  } else if (label == "Best") {
+    return "&#128077;";
+  } else if (label == "Victory") {
+    return "&#9996;";
+  }
 }
